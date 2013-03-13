e0d71365db1a54efd48aec50091d630146650bbd
hengxti <m.hengstberger@gmx.at>
2013-03-13 15:42:32 +0100

    formated

--- src/com/tdt4240/A6/junglearena/activities/JungleArenaActivity.java
+++ src/com/tdt4240/A6/junglearena/activities/JungleArenaActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.tdt4240.A6.junglearena.controller.JungleArena;
+import com.tdt4240.A6.junglearena.JungleArena;
 
 public class JungleArenaActivity extends AndroidApplication {
 	public void onCreate(Bundle savedInstanceState) {
--- src/com/tdt4240/A6/junglearena/model/Physics.java
+++ src/com/tdt4240/A6/junglearena/model/Physics.java
@@ -7,6 +7,7 @@ public class Physics {
 	private double power;
 	private double gravity;
 	private Vector3 wind;
+
 	public Physics(double degree, double power, double gravity, Vector3 wind) {
 		super();
 		this.degree = degree;
@@ -14,27 +15,35 @@ public class Physics {
 		this.gravity = gravity;
 		this.wind = wind;
 	}
+
 	public double getDegree() {
 		return degree;
 	}
+
 	public void setDegree(double degree) {
 		this.degree = degree;
 	}
+
 	public double getPower() {
 		return power;
 	}
+
 	public void setPower(double power) {
 		this.power = power;
 	}
+
 	public double getGravity() {
 		return gravity;
 	}
+
 	public void setGravity(double gravity) {
 		this.gravity = gravity;
 	}
+
 	public Vector3 getWind() {
 		return wind;
 	}
+
 	public void setWind(Vector3 wind) {
 		this.wind = wind;
 	}
