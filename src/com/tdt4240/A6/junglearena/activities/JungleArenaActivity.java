644312139ca707f5ffe075f7d535ad7bdf807738
hengxti <m.hengstberger@gmx.at>
2013-03-13 14:32:38 +0100

    first attempt

---
+++ src/com/tdt4240/A6/junglearena/activities/JungleArenaActivity.java
@@ -0,0 +1,20 @@
+package com.tdt4240.A6.junglearena.activities;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tdt4240.A6.junglearena.controller.JungleArena;
+
+public class JungleArenaActivity extends AndroidApplication {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		config.useWakelock = true;
+		config.useGL20 = true;
+		initialize(new JungleArena(), config);		
+	}
+
+}
