892fcc6401d8eb22e5d10d7b2fa31545ad655773
hengxti <m.hengstberger@gmx.at>
2013-03-13 15:48:03 +0100

    map added

---
+++ src/com/tdt4240/A6/junglearena/model/Map.java
@@ -0,0 +1,18 @@
+package com.tdt4240.A6.junglearena.model;
+
+public class Map {
+	private String Name;
+
+	public Map(String name) {
+		Name = name;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+}
