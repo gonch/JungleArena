058cd753c214a41dcf2e2c2e268d4c3777b2d2b6
hengxti <m.hengstberger@gmx.at>
2013-03-13 14:33:35 +0100

    added controller

---
+++ src/com/tdt4240/A6/junglearena/controller/JungleArena.java
@@ -0,0 +1,13 @@
+package com.tdt4240.A6.junglearena.controller;
+
+import com.badlogic.gdx.Game;
+
+
+public class JungleArena extends Game{
+
+	@Override
+	public void create() {
+//		setScreen(); // TODO
+	}
+
+}
