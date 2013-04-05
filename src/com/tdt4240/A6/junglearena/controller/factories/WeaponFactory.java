a084c83b59d367919bcbad791e8bd71a2245004d
hengxti <m.hengstberger@gmx.at>
2013-04-05 16:55:24 +0200

    added Weapons factory

---
+++ src/com/tdt4240/A6/junglearena/controller/factories/WeaponFactory.java
@@ -0,0 +1,17 @@
+package com.tdt4240.A6.junglearena.controller.factories;
+
+import com.tdt4240.A6.junglearena.model.Weapons.*;
+
+public class WeaponFactory {
+
+	public Weapon createWeapon(String type, int damage, String name, String skin, int areaOfEffect){
+		switch(type){
+		case "bomb": return new Bomb(damage, name, skin, areaOfEffect);
+		case "gun":  return new Gun(damage, name, skin, areaOfEffect);
+		case "bazuka": return new Bazuka(damage, name, skin, areaOfEffect);
+		// Add more Weapons here !
+		default: System.err.print("invalid Weapon. Can not create \""+type+"\"!");
+				return null;
+		}
+	}
+}
