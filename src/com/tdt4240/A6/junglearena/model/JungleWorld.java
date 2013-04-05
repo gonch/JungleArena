4ec460f535d6cee0c91cbfd7630e1b2d4a644181
hengxti <m.hengstberger@gmx.at>
2013-04-05 16:56:46 +0200

    edited the weapon creation according to the new weaponfactory

--- src/com/tdt4240/A6/junglearena/model/JungleWorld.java
+++ src/com/tdt4240/A6/junglearena/model/JungleWorld.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.tdt4240.A6.junglearena.model.Weapons.Weapon;
 
 
 public class JungleWorld{
