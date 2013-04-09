9ef5da0730dd46960121098cd9ee78d685b08911
AlessioDeAngelis <ale.deangelis89@gmail.com>
2013-04-09 15:20:14 +0200

    LIBGDX ON ANDRODI DOESN'T SUPPORT JAVA 7!

--- src/com/tdt4240/A6/junglearena/controller/factories/CharacterFactory.java
+++ src/com/tdt4240/A6/junglearena/controller/factories/CharacterFactory.java
@@ -6,14 +6,16 @@ import com.tdt4240.A6.junglearena.model.characters.Character;
 
 public class CharacterFactory {
 
-	public Character createCharacter(String type,int health, String name, Vector2 pos, String skin){
-		switch(type){
-		case "elephant": return new Elephant(health, name, pos, skin);
-		case "monkey" : return new Monkey(health, name, pos, skin);
-		case "turtle" : return new Turtle(health, name, pos, skin);
-		// Add more Characters here, if needed
-		default: System.err.print("invalid Character. Can not create \""+type+"\"!Created default instead.");
-					return new Monkey(health, name, pos, skin);
+	public Character createCharacter(String type, int health, String name, Vector2 pos, String skin) {
+
+		if (type != null && type.equals("elephant")) {
+			return new Elephant(health, name, pos, skin);
+		} else if (type != null && type.equals("monkey")) {
+			return new Monkey(health, name, pos, skin);
+		} else if (type != null && type.equals("turtle")) {
+			return new Elephant(health, name, pos, skin);
 		}
+		System.err.print("invalid Character. Can not create \"" + type + "\"!Created default instead.");
+		return null;
 	}
 }
--- src/com/tdt4240/A6/junglearena/controller/factories/WeaponFactory.java
+++ src/com/tdt4240/A6/junglearena/controller/factories/WeaponFactory.java
@@ -1,17 +1,23 @@
 package com.tdt4240.A6.junglearena.controller.factories;
 
 import com.tdt4240.A6.junglearena.model.Weapons.*;
+import com.tdt4240.A6.junglearena.model.characters.Elephant;
 
 public class WeaponFactory {
 
 	public Weapon createWeapon(String type, int damage, String name, String skin, int areaOfEffect){
-		switch(type){
-		case "bomb": return new Bomb(damage, name, skin, areaOfEffect);
-		case "gun":  return new Gun(damage, name, skin, areaOfEffect);
-		case "bazuka": return new Bazuka(damage, name, skin, areaOfEffect);
-		// Add more Weapons here, if needed.
-		default: System.err.print("invalid Weapon. Can not create \""+type+"\"!Created default instead.");
+		
+		if (type != null && type.equals("bomb")) {
 			return new Bomb(damage, name, skin, areaOfEffect);
-		}
+		} 
+		if (type != null && type.equals("gun")) {
+			return new Gun(damage, name, skin, areaOfEffect);
+		} 
+		if (type != null && type.equals("bazuka")) {
+			return new Bazuka(damage, name, skin, areaOfEffect);
+		} 
+		 System.err.print("invalid Weapon. Can not create \""+type+"\"!Created default instead.");
+		return null;
+		
 	}
 }
