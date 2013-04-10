f6ac71f6f1d67e22d1cb5556019afd3fb07e1fb8
hengxti <m.hengstberger@gmx.at>
2013-04-10 11:47:24 +0200

    added Linked list for weapons

--- src/com/tdt4240/A6/junglearena/controller/factories/WeaponFactory.java
+++ src/com/tdt4240/A6/junglearena/controller/factories/WeaponFactory.java
@@ -1,17 +1,48 @@
 package com.tdt4240.A6.junglearena.controller.factories;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import com.tdt4240.A6.junglearena.model.Weapons.*;
 
 public class WeaponFactory {
+	private List<String> weapons;
+	private final String pkgPath = "com.tdt4240.A6.junglearena.model.weapons";
 
-	public Weapon createWeapon(String type, int damage, String name, String skin, int areaOfEffect){
-		switch(type){
-		case "bomb": return new Bomb(damage, name, skin, areaOfEffect);
-		case "gun":  return new Gun(damage, name, skin, areaOfEffect);
-		case "bazuka": return new Bazuka(damage, name, skin, areaOfEffect);
-		// Add more Weapons here, if needed.
-		default: System.err.print("invalid Weapon. Can not create \""+type+"\"!Created default instead.");
-			return new Bomb(damage, name, skin, areaOfEffect);
+	public WeaponFactory() {
+		weapons = new LinkedList<String>();
+		weapons.add("bomb");
+		weapons.add("gun");
+		weapons.add("bazuka");
+	}
+
+	public String[] getWeapons() {
+		return (String[]) weapons.toArray();
+	}
+
+	public Weapon createWeapon(String type, int damage, String name, String skin, int areaOfEffect) {
+		for (String s : weapons) {
+			if (s.equals(type.toLowerCase())) {
+				// ClassLoader WeaponClassLoader =
+				// ClassLoader.getSystemClassLoader();
+				// String className = pkgPath + type;
+				// Class<?> w = WeaponClassLoader.loadClass(className);
+				//
+				// Object WeaponInstance = w.
+
+				// return new class (type) with damage, name, skin,
+				// areaOfEffect)
+			}
 		}
+
+		// switch(type){
+		// case "bomb": return new Bomb(damage, name, skin, areaOfEffect);
+		// case "gun": return new Gun(damage, name, skin, areaOfEffect);
+		// case "bazuka": return new Bazuka(damage, name, skin, areaOfEffect);
+		// // Add more Weapons here, if needed.
+		// default:
+		// System.err.print("invalid Weapon. Can not create \""+type+"\"!Created default instead.");
+		// return new Bomb(damage, name, skin, areaOfEffect);
+		// }
 	}
 }
